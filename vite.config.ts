e53a5f120d1df4753510216de8333dca858a37be
xandrevv <andrewglenn04@gmail.com>
2026-08-22 11:03:09 -0600

    switch to nitro Vercel preset for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import { devtools } from '@tanstack/devtools-vite'
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
+import { nitro } from 'nitro/vite'
 import viteReact from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
@@ -9,10 +10,9 @@ const config = defineConfig({
   plugins: [
     devtools(),
     tailwindcss(),
-    tanstackStart({
-      deployment: {
-        preset: 'vercel',
-      },
+    tanstackStart(),
+    nitro({
+      preset: 'vercel',
     }),
     viteReact(),
   ],
